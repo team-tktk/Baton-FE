cb67e87555099c520ae7df0ab457fb2f0bd5f40c
DohyunKim <rlaehgus4418@naver.com>
2026-08-25 22:47:33 +0900

    fix: 리뷰 상세 화면을 데모 UI에 맞게 개선

--- src/widgets/handover-document/index.ts
+++ src/widgets/handover-document/index.ts
@@ -1,2 +1,3 @@
 export { HandoverDocument } from './ui/HandoverDocument'
 export { HandoverDraftEditor } from './ui/HandoverDraftEditor'
+export { HandoverReadDocument } from './ui/HandoverReadDocument'
